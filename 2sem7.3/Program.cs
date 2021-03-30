fbaec719be16105f613d582e7cc27b8ed3352aaf
Ivan Dzherikhov <jodsonjod@gmail.com>
2021-03-30 20:02:03 +0300

    Добавьте файлы проекта.

---
+++ 2sem7.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2sem7._3
+{
+    class Program
+    {
+        delegate void IsDivisible(int x);
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(0, 100);
+            foreach (int i in arr) 
+            {
+                IsDivisible oper = Divide;
+                oper.Invoke(i);
+            }
+            Console.ReadKey();
+        }
+       
+        public static void Divide(int x) 
+        { 
+            if (x % 3 == 0 || x % 7 == 0)
+            {
+                
+                Console.WriteLine(x);
+            }
+            
+        }   
+    }
+
+}
